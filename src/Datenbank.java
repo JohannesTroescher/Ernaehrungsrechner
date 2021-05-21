c6e269a27ec5ba37824daaadc92c44d3c925dbf4
Johannes Tröscher <johannes.thomas.troescher@hs-furtwangen.de>
2021-05-21 10:13:31 +0200

    1ter test

---
+++ src/Datenbank.java
@@ -0,0 +1,7 @@
+public class Datenbank {
+    /*Hallo Luc kannst du das hier lesen?
+    wenn du das hier offen hast und es siehst dann mach doch bitte mal ne Änderung und versuch das dann zu pushen.
+    wenn ich von Chemie wieder zurück bin schau ich mal ob es funktioniert hat.
+    Erstell auch mal probehalber ne neue Klasse um zu schauen ob auch das funktioniert.
+     */
+}
