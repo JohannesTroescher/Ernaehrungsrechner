c37fdf94e9d85c0729477403771311d875860159
Luc <luc-silvan.uhlig@hs-furtwangen.de>
2021-05-21 19:05:25 +0200

    Enjoy

--- src/Datenbank.java
+++ src/Datenbank.java
@@ -1,7 +1,24 @@
+import javax.xml.transform.stream.StreamSource;
+
 public class Datenbank {
     /*Hallo Luc kannst du das hier lesen?
     wenn du das hier offen hast und es siehst dann mach doch bitte mal ne Änderung und versuch das dann zu pushen.
     wenn ich von Chemie wieder zurück bin schau ich mal ob es funktioniert hat.
     Erstell auch mal probehalber ne neue Klasse um zu schauen ob auch das funktioniert.
      */
+
+    /*Alles lesbar, mal schauen ob auch das pushen und updaten funktioniert.*/
+
+
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        int b = 1;
+        int c;
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Loading very nice Cock: " +i+ "% done");
+        }
+        System.out.println("Nice Cock loaded and ready for usage!");
+    }
 }
