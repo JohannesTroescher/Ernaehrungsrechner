2eef24018dbd9d2a84fd3259bf80ca74668aaabf
Jannis <jannis.hofmann@web.de>
2021-06-03 11:56:38 +0200

    Erste Version UserDaten und Rezepte

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         UserDaten Dieter= new UserDaten("Dieter Müller",36,1.87,"männlich","Muskelaufbau","normal",80);
         UserDaten Marianne= new UserDaten("Marianne Müller",35,1.87,"weiblich","Abnehmen","normal",50);
-
     System.out.println(Dieter.getGrundbedarf());
     System.out.println(Marianne.getGrundbedarf());
     }
