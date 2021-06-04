a9589fc5a26b9410a91be2403153fc231d6fb7d5
frankgit-hub <84561221+frankgit-hub@users.noreply.github.com>
2021-06-04 08:43:08 +0200

    test

--- src/Interface.java
+++ src/Interface.java
@@ -5,39 +5,60 @@ public class Interface {
     private int Kalorien_sollwert;
     private String Rezeptvorschläge;
 
-    /**                     Konstruktor Interface identisch mit UserDaten?
-     * @param name          Name des Nutzers
-     * @param alter         Alter des Nutzers
-     * @param groesse       Groeße des Nutzers
-     * @param geschlecht    Geschlecht Alter des Nutzers
-     * @param training      Gewünschter Modus: Abnehmen/Muskelaufbau
-     * @param nahrung       Gewünsche Nahrungsart: normal, vegetarisch, vegan
-     * @param gewicht       Gewicht Alter des Nutzers
+    /**
+     * Konstruktor Interface identisch mit UserDaten?
+     *
+     * @param
+
      */
-    public Interface (String name,int alter,double groesse,String geschlecht, String training,String nahrung,double gewicht){}
+    public Interface(int Kalorien_sollwert,String Rezeptvorschläge) {
+        this.Kalorien_sollwert=Kalorien_sollwert;
+        this.Rezeptvorschläge= Rezeptvorschläge;
+    }
 
     /**
      * Standardkonstruktor der Klasse Interface
      */
-    public Interface(){}
+    public Interface() {
+    }
 
-    public String User_erstellen(String UserID){
+    public String getRezeptvorschläge() {
+        return Rezeptvorschläge;
+    }
+
+    public void setRezeptvorschläge(String rezeptvorschläge) {
+        Rezeptvorschläge = rezeptvorschläge;
+    }
+
+    public int getKalorien_sollwert() {
+        return Kalorien_sollwert;
+    }
+
+    public void setKalorien_sollwert(int kalorien_sollwert) {
+        Kalorien_sollwert = kalorien_sollwert;
+    }
+
+    public String User_erstellen(String UserID) {
 //Erstellt einen neuen User
         return UserID;
     }
 
-    public void Information_display(){}
+
+    public void Information_display() {
+    }
 //ruft Rezepte auf, und gibt aufgerufene Rezepte an GUI um darzustellen
 
-    public String User_aufruf(String UserID){
+
+    public String User_aufruf(String UserID) {
 //ruft die die Daten des entsprechenden Users auf (aus UserDatenDatei über Userdaten_auslesen())
         return UserID;
     }
 
-    public String User_änderung(String UserID){
+    public String User_änderung(String UserID) {
 //geänderte User/null(wenn User nicht existiert)
 //Funktionsweise: ersetzt alte Userdaten mit neuen, ruft Userdaten_aendern() auf wenn nicht null
         return UserID;
     }
+
 
 }
