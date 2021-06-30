eabd597d5372352c24ac8323df3c50522ba0f731
frankgit-hub <84561221+frankgit-hub@users.noreply.github.com>
2021-06-30 22:25:36 +0200

    gui

--- src/Gui.java
+++ src/Gui.java
@@ -12,10 +12,10 @@ public class Gui extends Frame
         }
     }
 
-    public Gui()
+    public void Fenster()
     {
         setTitle("Ernährungsrechner");
-        setSize(720,720);
+        setSize(720,440);
         addWindowListener(new Fensterereignisse());
         setLayout(new FlowLayout());
         Panel panel = new Panel();
@@ -32,7 +32,6 @@ public class Gui extends Frame
         f3 = new TextField("0",3);
         panel.add(new Label(" Gewicht"));
         panel.add(f3);
-        setVisible(true);
 
         f4 = new TextField("0",3);
         panel.add(new Label(" Größe"));
@@ -40,11 +39,20 @@ public class Gui extends Frame
         add(panel);
         setVisible(true);
 
-        Choice choice = new Choice();
-        choice.addItem("weiblich");
-        choice.addItem("männlich");
-        this.add(choice);
+        Choice choice1 = new Choice();
+        choice1.addItem("weiblich");
+        choice1.addItem("männlich");
+        this.add(choice1);
 
+        Choice choice2 = new Choice();
+        choice2.addItem("Normal");
+        choice2.addItem("Vegetarisch");
+        choice2.addItem("Vegan");
+        this.add(choice2);
+
+        Choice choice3 = new Choice();
+        choice3.addItem("Abnehmen");
+        choice3.addItem("Muskelaufbau");
+        this.add(choice3);
     }
 }
-
